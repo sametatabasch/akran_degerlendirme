0e416ca1b3cded614ff6870ee245b36987551d79
sametatabasch <sametatabasch@gmail.com>
2023-12-01 23:53:42 +0300

    setup.py eklendi

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+
+setup(
+    name='akran_degerlendirme',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/sametatabasch/akran_degerlendirme',
+    license='MIT',
+    author='sametatabasch',
+    author_email='sametatabasch@gmail.com',
+    description='',
+    install_requires=[
+        'Flask',
+        'flask_bcrypt',
+        'flask_login',
+        'flask_sqlalchemy'
+    ],
+    entry_points={
+        'console_scripts': [
+
+        ],
+    },
+    # bdist_wheel parametresi eklendi
+    setup_requires=[
+        'wheel',
+    ],
+)
